94fd236f6a8d6e32cf323695d0b742a93793b69c
Chocoffees <sirogrenadine@gmail.com>
2022-04-19 00:14:38 +0100

    File updated

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,34 @@
 // Import Express
 const express = require('express');
 
+// Import routers
+const userRoutes = require('./routes/user');
+
 // Create Express application
 const app = express();
 
-// Middleware which analyse json requests 
-app.use(express.json());
+// Middleware which analyse json requests/parse incoming req
+app.use(express.json()); // json
+app.use(express.urlencoded({ extended: true })); // string
+
+/*test route
+app.get('/', (req, res) => {
+    res.json({ message: "test OK!" });
+});*/
+
+// Security CORS: add middleware applicable to all routes, requests to server // CORS: prevent malicious requests
+app.use((req, res, next) => {
+    // Allow to access a resource > any origin '*'
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Headers authorized for requests
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // Allow following mentionned methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Save routes
+app.use('/api/user', userRoutes);
 
 // Export the application
 module.exports = app;
