fbec43a4aa317e5c9adc44b2aff89fae8b6ce7e1
Chocoffees <sirogrenadine@gmail.com>
2022-06-03 08:05:00 +0100

    List of posts - review in progress

--- groupomania/src/components/ListOfPosts.vue
+++ groupomania/src/components/ListOfPosts.vue
@@ -5,69 +5,101 @@
   <!-- render list of posts -->
   <div class="post" v-for="post in posts" v-bind:key="post.id">
     <div class="post-header">
-      <img src="../assets/Mee.png" alt="Avatar miniature" /><strong>{{ post.User.username }}</strong><br />
+      <img src="../assets/Mee.png" alt="Avatar miniature" /><strong>{{ post.User.username }}</strong><br/>
       <p>- mis à jour le {{ post.updatedAt }}</p>
-      <!-- accept? -->
-      <router-link :to="'/posts/' + post.id">
-        <button 
+
+      <!-- once user is logged: show available functions > edit & delete -->
+      <div v-if="user.user.id == post.UserId">
+        <button
+          @click="editPost(post.id)"
           aria-label="modifier ma publication"
           title="Modifier"
           class="post-edit"
         >
           ✏️
         </button>
-      </router-link>
-      <!-- to check! -->
-      <!--<button @click="editPost" aria-label="modifier ma publication" title="Modifier" class="post-edit">✏️</button>-->
-      <router-link :to="'/posts'">
-      <button
-        @click="deletePost(post.id, post.UserId)"
-        aria-label="supprimer ma publication"
-        title="Supprimer"
-        class="post-delete"
-      >
-        ❌
-      </button>
-      </router-link>
+        <button
+          @click="deletePost(post.id, post.UserId)"
+          aria-label="supprimer ma publication"
+          title="Supprimer"
+          class="post-delete"
+        >
+          ❌
+        </button>
+      </div>
     </div>
 
+    <!-- post content -->
     <div class="post-content">
       <h2>{{ post.title }}</h2>
       <p>{{ post.content }}</p>
-      <img :src="post.attachment"/>
+      <img :src="post.attachment" />
     </div>
-  </div>
+  
 
+  <!-- post-comment: interact -->
+  <button
+          @click="commentPost"
+          aria-label="commenter la publication"
+          title="Commenter"
+          class="post-comment"
+        >
+          🗪 <p>Ajouter un commentaire</p>
+        </button><br>
+  
+  <div class="comment">
+    <img src="../assets/Mee.png" alt="Avatar miniature" /><strong>{{ user.user.username }}</strong><br/>
+      <p>- le {{  }}</p>
+  </div>
+  </div>
 </template>
 
 <script>
 // Import 'axios' > perform API requests (used to send/get data from API)
 import axios from "axios";
+// Getters from auth module
+import { mapGetters } from "vuex";
 
 export default {
   name: "ListOfPosts",
 
+  computed: {
+    ...mapGetters({
+      isAuthenticated: "auth/isAuthenticated",
+      user: "auth/user",
+    }),
+  },
+
   data() {
     return {
       posts: [],
-      }
+    };
+  },
+
+  methods: {
+    // Call function 'editPost()' > direct user to 'updatePost'
+    editPost(id) {
+      let actualPost = id;
+      console.log("Ready to ✏️ > Post n°", actualPost);
+      this.$router.push(`/posts/${id}`);
     },
 
-    methods: {
-    /*editPost() { 
-      this.$router.push(`/posts/${id}`);  
-      },*/
     // Call function 'deletePost()'
     // Check post before action
     async deletePost(id, UserId) {
       let postToDelete = id;
       let postOwner = UserId;
-      console.log('Ready to ❌ > Post n°', postToDelete, '- User:', postOwner);
+      console.log("Ready to ❌ > Post n°", postToDelete, "- User:", postOwner);
 
-      if(confirm('ℹ️ La supression du message est irréversible. Voulez-vous continuer ?')) {
+      if (
+        confirm(
+          "ℹ️ La supression du message est irréversible. Voulez-vous continuer ?"
+        )
+      ) {
         await axios.delete("http://localhost:8080/api/posts/" + id);
-        console.log('Post n°', id, 'now destroyed')
-        .then(function (response) {
+        console
+          .log("Post n°", id, "now destroyed")
+          .then(function (response) {
             console.log(response);
             window.location.reload();
           })
@@ -75,7 +107,7 @@ export default {
             console.log(error);
           });
       }
-    }
+    },
   },
 
   async mounted() {
@@ -85,12 +117,10 @@ export default {
     console.log(this.posts);
   },
 };
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .welcome {
   background-color: rgb(209 81 90 / 10%);
 }
@@ -171,5 +201,30 @@ nav a.router-link-exact-active {
 .post-content > img {
   max-width: 100%;
   height: auto;
+}
+.post-comment {
+  align-items: center;
+  display: flex;
+  background: none;
+  border: none;
+  color: brown;
+  cursor: pointer;
+  font-size: 2em;
+  padding: 15px 5px 5px 15px;
+}
+.post-comment > p {
+  color: black;
+  font-size: medium;
+  margin-left: 10px;
+}
+.comment > img {
+  height: 30px;
+  margin-right: 5px;
+  width: 30px;
+}
+.comment > p {
+  color: gray;
+  font-size: smaller;
+  margin-left: 5px;
 }
 </style>
