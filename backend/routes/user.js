4be08daaf34ff35c543caa30c93a8998f58f15f1
Chocoffees <sirogrenadine@gmail.com>
2022-07-01 10:50:00 +0100

    Handle user avatar

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,6 +9,9 @@ const router = express.Router();
 // Import authentication middleware > protect routes
 const auth = require('../middlewares/auth');
 
+// Import multer middleware > handle images uploaded
+const multer = require('../middlewares/multer-config_user');
+
 // Create controller > associate functions to different routes
 const userCtrl = require('../controllers/user');
 
@@ -19,7 +22,7 @@ router.post('/login', userCtrl.login);
 // Get one user
 router.get('/myaccount/:id', auth, userCtrl.getOneUser);
 // Update user data
-router.put('/myaccount-update/:id', auth, userCtrl.updateOneUser);
+router.put('/myaccount-update/:id', auth, multer, userCtrl.updateOneUser);
 // Delete user account > remove from database
 router.delete('/myaccount/:id', auth, userCtrl.deleteUser);
 // Get all users
